f2d374ee989699ae295982aa1df8779e760f9dee
Heer Patel <heerpatel1130@gmail.com>
2024-06-10 12:01:44 +0530

    all error solve

--- anime/src/Components/Header/Header.js
+++ anime/src/Components/Header/Header.js
@@ -7,18 +7,23 @@ const Header = () => {
 
   const onSearch = async (e) => {
     e.preventDefault();
-    setSearch(e.target.value);
-    getData();
+    const searchText = e.target.value;
+    setSearch(searchText);
   };
 
-  const getData = async () => {
+  const getData = async (searchText) => {
     try {
-      const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}`);
+      const res = await fetch(`https://api.jikan.moe/v4/anime?q=${searchText}`);
       const data = await res.json();
-      setSearchResults(data.results);
+      console.log(data);
+      setSearchResults(data.data);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const handleSearch = () => {
+    getData(search);
   };
 
   return (
@@ -35,18 +40,23 @@ const Header = () => {
             placeholder='Search Anime'
             value={search}
             onChange={onSearch}
+            onBlur={handleSearch} // Trigger search when input loses focus
           />
         </div>
       </div>
-      
-      {searchResults.map((anime) => (
-        <div className="card" key={anime.id}>
-          <img src={anime.image_url} className="card-img-top" alt={anime.title} />
+      <div className='card-body'>
+
+      {searchResults && searchResults.length > 0 && searchResults.map((anime) => (
+        // console.log(anime)
+        <div className="card" key={anime.mal_id
+          }>
+          <img src={anime.images.jpg.image_url} className="card-img-top" alt={anime.title} />
           <div className="card-body">
             <p className="card-text">{anime.title}</p>
           </div>
         </div>
       ))}
+    </div>
     </>
   );
 };
